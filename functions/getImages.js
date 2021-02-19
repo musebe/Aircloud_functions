2780d9846d1c644cd73b394ff8ee4379894c4f8e
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro.local>
2021-02-19 18:21:21 +0300

    added cors callback

--- functions/getImages.js
+++ functions/getImages.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { table } = require('./utils/airtable');
 
-exports.handler = async (event) => {
+exports.handler = async (event,context,callback) => {
   try {
     const records = await table.select({}).firstPage();
     const formattedRecords = records
@@ -20,4 +20,11 @@ exports.handler = async (event) => {
       body: JSON.stringify({ err: 'Failed to upload image' }),
     };
   }
+  callback(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: output,
+  });
 };
